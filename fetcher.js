a4e97089f4cd7d261cd07890ae54fa3b4e8ec5ce
zezking <zhaoenze001@gmail.com>
2021-02-11 05:31:46 +0000

    refactoring

--- fetcher.js
+++ fetcher.js
@@ -1,35 +1,41 @@
+const fs = require("fs");
+const request = require("request");
+const readline = require("readline");
+
 const link = process.argv.slice(2)[0];
 const filePath = process.argv.slice(2)[1];
 const fileName = filePath.slice(2, filePath.length);
-const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const fs = require("fs");
-
-console.log(fileName);
-
-var request = require("request");
 request(link, function (error, response, body) {
   if (error && response.statusCode == 200) {
     console.log("There is an error");
+    console.log(error);
   }
   fs.access(filePath, fs.F_OK, (error) => {
     if (error) {
       fs.writeFile(filePath, body, (error) => {
-        if (error) throw error;
-        const fileSize = fs.statSync(fileName).size;
-        console.log(`Downloaded and saved ${fileSize} bytes to ${filePath}`);
+        if (error) {
+          throw error;
+        }
+        console.log(
+          `Downloaded and saved ${
+            fs.statSync(fileName).size
+          } bytes to ${filePath}`
+        );
+        process.exit();
       });
     } else {
       rl.question("File exits and rewrite? y/n   ", (answer) => {
         if (answer === "y" || answer === "Y") {
-          const fileSize = fs.statSync(fileName).size;
-          console.log(`Downloaded and saved ${fileSize} bytes to ${filePath}`);
-        } else if (answer === "n" || answer === "N") {
-          rl.close();
+          console.log(
+            `Downloaded and saved ${
+              fs.statSync(fileName).size
+            } bytes to ${filePath}`
+          );
         }
         rl.close();
       });
