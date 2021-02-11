3367d6e1cf228781182604ef7f5e0c3f8b3dcb85
zezking <zhaoenze001@gmail.com>
2021-02-11 04:20:59 +0000

    update user interface to prompt user if rewrite file

--- fetcher.js
+++ fetcher.js
@@ -1,6 +1,12 @@
 const link = process.argv.slice(2)[0];
 const filePath = process.argv.slice(2)[1];
 const fileName = filePath.slice(2, filePath.length);
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const fs = require("fs");
 
 console.log(fileName);
@@ -10,12 +16,23 @@ request(link, function (error, response, body) {
   if (error && response.statusCode == 200) {
     console.log("There is an error");
   }
-  fs.writeFile(filePath, body, (error) => {
-    if (error) throw error;
-    console.log("The page has been saved!");
+  fs.access(filePath, fs.F_OK, (error) => {
+    if (error) {
+      fs.writeFile(filePath, body, (error) => {
+        if (error) throw error;
+        const fileSize = fs.statSync(fileName).size;
+        console.log(`Downloaded and saved ${fileSize} bytes to ${filePath}`);
+      });
+    } else {
+      rl.question("File exits and rewrite? y/n   ", (answer) => {
+        if (answer === "y" || answer === "Y") {
+          const fileSize = fs.statSync(fileName).size;
+          console.log(`Downloaded and saved ${fileSize} bytes to ${filePath}`);
+        } else if (answer === "n" || answer === "N") {
+          rl.close();
+        }
+        rl.close();
+      });
+    }
   });
 });
-
-const fileSize = fs.statSync("index1.html").size;
-
-console.log(`Downloaded and saved ${fileSize} bytes to ${filePath}`);
